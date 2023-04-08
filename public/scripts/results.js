d1aa930bb635f531ef4fe529610b9cebd021733a
solomonmihai <solomonmihai10@gmail.com>
2023-04-08 18:38:23 +0300

    fixed movie page; changed results page

--- public/scripts/results.js
+++ public/scripts/results.js
@@ -3,23 +3,28 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const container = document.getElementById("results");
 
-  const url = "https://api.themoviedb.org/3/discover/movie?api_key=81a9dfa33886cd2fd7f3d3dd2e1302db&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=50";
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?api_key=81a9dfa33886cd2fd7f3d3dd2e1302db&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=50";
 
   const response = await fetch(url);
   const data = await response.json();
 
   for (let i = 0; i < items; i++) {
+    const { poster_path, original_title } = data.results[i];
 
-    const { poster_path, original_title, overview } = data.results[i];
+    const el = document.createElement("div");
+    el.classList.add("box");
+    el.onclick = () => {
+      location.href = "movie.html"
+    }
 
-    const el = `
-      <div onclick="location.href='movie.html';" style="cursor: pointer;" class="box">
-        <img class="result-img" src=${"https://image.tmdb.org/t/p/w300" + poster_path} alt="result"/>
+    el.innerHTML = `
+      <img class="bg-img" src="https://image.tmdb.org/t/p/w300${poster_path}"/>
+      <div class="movie-desc" onclick=";" style="cursor: pointer;" class="box">
         <h3 class="result-title">${original_title}</h3>
-        <p class="result-desc">${overview}</p>
       </div>
     `;
 
-    container.innerHTML += el;
+    container.appendChild(el);
   }
 });
