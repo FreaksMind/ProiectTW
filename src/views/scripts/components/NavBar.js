58e8e87c1997d6fbc31944d58b8119ff94b2b857
solomonmihai <solomonmihai10@gmail.com>
2023-06-22 20:54:32 +0300

    added profile button

--- src/views/scripts/components/NavBar.js
+++ src/views/scripts/components/NavBar.js
@@ -5,19 +5,6 @@ import "./SearchBar.js";
 const template = document.createElement("template");
 template.innerHTML = `
 <style>
-  .logout-button {
-    color: white;
-    background-color: transparent;
-    border: none;
-    font-size: 1rem;
-    cursor: pointer;
-    font-family: var(--rubik);
-  }
-
-  .logout-button:hover {
-    color: var(--accent-color);
-    text-decoration: underline;
-  }
 
   .wrapper {
     z-index: 100;
@@ -90,6 +77,28 @@ template.innerHTML = `
     }
   }
 
+  #account-btn {
+    color: white;
+    background-color: transparent;
+    border: none;
+    font-size: 1rem;
+    cursor: pointer;
+    font-family: var(--rubik);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: rgb(140, 140, 140);
+    gap: 5px;
+    border-radius: 10px;
+    transition: 0.1s ease;
+    padding: 5px 10px;
+    text-decoration: none;
+  }
+
+#account-btn:hover {
+  background-color: rgb(30, 30, 30);
+}
+
 </style>
 
 <div class="wrapper">
@@ -102,7 +111,10 @@ template.innerHTML = `
     </div>
     <div id="search-bar-container"></div>
     <div>
-       <button id="logout-btn" class="logout-button">logout</button>
+      <a id="account-btn" href="/profile">
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" id="account" viewBox="0 0 48 48"><path fill="rgb(70, 70, 70)"  d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm0 6c3.31 0 6 2.69 6 6 0 3.32-2.69 6-6 6s-6-2.68-6-6c0-3.31 2.69-6 6-6zm0 28.4c-5.01 0-9.41-2.56-12-6.44.05-3.97 8.01-6.16 12-6.16s11.94 2.19 12 6.16c-2.59 3.88-6.99 6.44-12 6.44z"/><path fill="none" d="M0 0h48v48H0z"/></svg>
+        <span id="username-label"></span>
+      </a>
     </div>
   </div>
 <div/>
@@ -136,16 +148,20 @@ class NavBar extends HTMLElement {
   }
 
   connectedCallback() {
-    const { user, logout } = checkAuth();
+    const { user } = checkAuth();
 
     if (!user) {
       window.location.href = "/";
     }
 
+    console.log(user)
+
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
-    this.shadowRoot.getElementById("logout-btn").addEventListener("click", logout);
+    // this.shadowRoot.getElementById("logout-btn").addEventListener("click", logout);
+
+    this.shadowRoot.getElementById("username-label").innerText = user.username;
 
     if (!this.searchbar) {
       return;
